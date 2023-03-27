15e8bc6e743857c2e2d582722d9191e67a91605f
Tanimodori <unknowner2014@gmail.com>
2023-03-27 15:50:38 +0800

    refactor: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,19 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import pugVitePlugin from './pug/vite-plugin-pug';
-import { i18next, langs } from './pug/i18next';
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import vitePluginPugI18n from "./plugin/pug";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '~': resolve(__dirname, './node_modules'),
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        'en/index.html': resolve(__dirname, 'src/pages', 'index.en.pug'),
-        'fr/index.html': resolve(__dirname, 'src/pages', 'index.fr.pug'),
-        'en/test/index.html': resolve(
-          __dirname,
-          'src/pages/test',
-          'index.en.pug'
-        ),
-        'fr/test/index.html': resolve(
-          __dirname,
-          'src/pages/test',
-          'index.fr.pug'
-        ),
-      },
-    },
-  },
   plugins: [
-    pugVitePlugin({
-      langs,
-      i18next,
+    vitePluginPugI18n({
+      pages: {
+        glob: "./src/pages/**/*.pug",
+        root: resolve(__dirname, "src/pages"),
+      },
+      i18n: {
+        glob: "./src/language/*.json",
+      },
       locals: {},
+      options: {},
     }),
   ],
 });
